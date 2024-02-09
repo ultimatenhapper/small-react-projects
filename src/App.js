4950d95f73c8f58bf98b11135a6cbc2c452a841e
Javi <belikemike3057@hotmail.com>
2024-02-09 23:36:51 +0100

    Random color component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
-import Accordian from './accordian';
+import Accordian from './components/accordian';
+import RandonColor from './components/random-color';
 
 function App() {
   return (
     <div className='App'>
       <Accordian />
+      <RandonColor />
     </div>
   );
 }
