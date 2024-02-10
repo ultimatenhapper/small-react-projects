6b3bae8631fe2c9de59e19837a29b2cf53965ee8
Javi <belikemike3057@hotmail.com>
2024-02-10 17:51:42 +0100

    Load more products component

--- src/App.js
+++ src/App.js
@@ -1,16 +1,18 @@
 import './App.css';
 import Accordian from './components/accordian';
 import ImageSlider from './components/image-slider';
+import LoadMoreData from './components/load-more-data';
 import RandonColor from './components/random-color';
 import StartRating from './components/star-rating';
 
 function App() {
   return (
     <div className='App'>
-      <Accordian />
-      <RandonColor />
-      <StartRating numOfStars={10}/>
-      <ImageSlider url={'https://picsum.photos/v2/list'} limit={10} />
+      {/* <Accordian /> */}
+      {/* <RandonColor /> */}
+      {/* <StartRating numOfStars={10}/> */}
+      {/* <ImageSlider url={'https://picsum.photos/v2/list'} limit={10} /> */}
+      <LoadMoreData />
     </div>
   );
 }
---
+++ src/components/load-more-data/index.jsx
@@ -0,0 +1,68 @@
+import React, { useState, useEffect } from "react";
+
+import "./styles.css";
+
+const LoadMoreData = () => {
+  const [loading, setLoading] = useState(false);
+  const [products, setProducts] = useState([]);
+  const [count, setCount] = useState(0);
+  const [disableButton, setDisableButton] = useState(false);
+
+  const fetchProducts = async () => {
+    const url = `https://dummyjson.com/products?limit=20&skip=${
+      count === 0 ? 0 : count * 20
+    }`;
+    console.log('Url', url);
+    try {
+      setLoading(true);
+      const response = await fetch(url);
+      const data = await response.json();
+
+      if (data && data.products && data.products.length) {
+        setProducts((prevData) => [...prevData, ...data.products]);
+        setLoading(false);
+        console.log("Data ", data);
+      }
+    } catch (e) {
+      console.log("Error ", e);
+      setLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    fetchProducts();
+  }, [count]);
+
+  useEffect(() => {
+    if (products && products.length === 100) setDisableButton(true);
+  }, [products]);
+
+  if (loading) {
+    return <div>Loading products...</div>;
+  }
+
+  return (
+    <div className="load-more-container">
+      <div className="product-container">
+        {products && products.length ? (
+          products.map((product) => (
+            <div key={product.id} className="product">
+              <img src={product.thumbnail} alt={product.title} />
+              <p>{product.title}</p>
+            </div>
+          ))
+        ) : (
+          <div>No products</div>
+        )}
+      </div>
+      <div className="button-container">
+        <button disabled={disableButton} onClick={() => setCount(count + 1)}>
+          Load more products
+        </button>
+      </div>
+      {disableButton ? <div>You have reached 100 products</div> : null}
+    </div>
+  );
+};
+
+export default LoadMoreData;
